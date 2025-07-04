ff1bfd6386de119348a187c708140480067e7d6b
yakoub589 <tvgames589@gmail.com>
2025-07-05 01:44:38 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Yakoub', // Your name
+  ownerNumber: '639983257542', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
 };
